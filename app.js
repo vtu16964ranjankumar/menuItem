1a15068d1d7d4d8cf1015c0d9a6b919a9312e8b7
Ranjan Kumar <vtu16964@veltech.edu.in>
2024-02-02 00:29:25 +0530

    updated app.js

---
+++ app.js
@@ -0,0 +1,59 @@
+// items
+const menu =[{
+  id:1,
+  title:"buttermilk pancakes",
+  category: "breakfast",
+  price: 15.99,
+  img: "./images/item-1.jpg",
+  desc: `I'm baby woke mlkshk wolf bitter live-edge blue bottle,
+  hammock freegan copper mug whatever cold-pressed`,
+},
+{
+  id:2,
+  title: "dinner double",
+  category: "lunch",
+  price: 13.99,
+  image: ",/images/item-2.jpeg",
+  desc:`vaporwareiphone mumblecore selvage raw denim
+  slow-carb leggings gochujang helvetica man braid jianbing
+  . marfa thundercats`,
+},
+{
+  id:3,
+  title:"buttermilk pancakes",
+  category: "breakfast",
+  price: 15.99,
+  img: "./images/item-1.jpg",
+  desc: `I'm baby woke mlkshk wolf bitter live-edge blue bottle,
+  hammock freegan copper mug whatever cold-pressed`,
+},
+{
+  id:4,
+  title: "dinner double",
+  category: "lunch",
+  price: 13.99,
+  image: ",/images/item-2.jpeg",
+  desc:`vaporwareiphone mumblecore selvage raw denim
+  slow-carb leggings gochujang helvetica man braid jianbing
+  . marfa thundercats`,
+},
+{
+  id:5,
+  title:"buttermilk pancakes",
+  category: "breakfast",
+  price: 15.99,
+  img: "./images/item-1.jpg",
+  desc: `I'm baby woke mlkshk wolf bitter live-edge blue bottle,
+  hammock freegan copper mug whatever cold-pressed`,
+},
+{
+  id:6,
+  title: "dinner double",
+  category: "lunch",
+  price: 13.99,
+  image: ",/images/item-2.jpeg",
+  desc:`vaporwareiphone mumblecore selvage raw denim
+  slow-carb leggings gochujang helvetica man braid jianbing
+  . marfa thundercats`,
+}
+]
